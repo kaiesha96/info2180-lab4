6ed862a8ef1e91f4d29552f253cba15a0bb98412
kaiesha96 <32602164+kaiesha96@users.noreply.github.com>
2017-10-19 19:55:11 -0400

    Add files via upload

---
+++ maze.js
@@ -0,0 +1,6 @@
+window.onload = function()
+{
+  	var boundary1 = document.getElementById("boundary1");
+  	boundary1.addEventListener("mouseover", function(){
+  		boundary1.setAttribute("class", "boundary youlose");});	
+}
