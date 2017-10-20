818d716a95e18455ff86bcd2edadbaa25f60e685
Shaniel Wallace <shaniel.wallace@mymona.uwi.edu>
2017-10-20 01:43:16 +0000

    first commit

--- maze.js
+++ maze.js
@@ -1,17 +1,13 @@
 window.onload = function()
 {
-  	var boundary1 = document.getElementById("boundary1");
-  	boundary1.addEventListener("mouseover", function(){
-  		boundary1.setAttribute("class", "boundary youlose");});	
-}
 var allBoundaries = document.querySelectorAll("div#maze div.boundary");
-  	for (var x = 0; x < allboun.length; x++){
+  	for (var x = 0; x < allBoundaries.length; x++){
   		allBoundaries[x].addEventListener("mouseover", turnRed);
   	}
   }
   function turnRed() {
       var allBoundaries = document.querySelectorAll("div#maze div.boundary");
-      for (var x = 0; x < allboun.length; x++){
+      for (var x = 0; x < allBoundaries.length; x++){
           allBoundaries[x].setAttribute("class", "boundary youlose");
       }
   }
