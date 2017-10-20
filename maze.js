ea161d9f9fd9b7acb3444a6b72454a5cea0f6617
kaiesha96 <32602164+kaiesha96@users.noreply.github.com>
2017-10-19 20:01:03 -0400

    Update maze.js
    #2

--- maze.js
+++ maze.js
@@ -4,3 +4,14 @@ window.onload = function()
   	boundary1.addEventListener("mouseover", function(){
   		boundary1.setAttribute("class", "boundary youlose");});	
 }
+var allBoundaries = document.querySelectorAll("div#maze div.boundary");
+  	for (var x = 0; x < allboun.length; x++){
+  		allBoundaries[x].addEventListener("mouseover", turnRed);
+  	}
+  }
+  function turnRed() {
+      var allBoundaries = document.querySelectorAll("div#maze div.boundary");
+      for (var x = 0; x < allboun.length; x++){
+          allBoundaries[x].setAttribute("class", "boundary youlose");
+      }
+  }
